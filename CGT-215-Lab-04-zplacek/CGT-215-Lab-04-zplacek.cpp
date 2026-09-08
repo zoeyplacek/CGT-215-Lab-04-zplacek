a4e21ccc8081e4454f4ff8c986bcc70946bf64b4
zplacek <zplacek@X-KNOY416PC25>
2026-09-08 11:24:09 -0400

    Add project files.

---
+++ CGT-215-Lab-04-zplacek/CGT-215-Lab-04-zplacek.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+// Print out the menu of choices for the user to select from
+void printMenu() {
+	cout << "Please Select which operation to perform:" << endl;
+	cout << "\t1. Factorial" << endl;
+	cout << "\t2. Arithmetic Series" << endl;
+	cout << "\t3. Geometric Series" << endl;
+	cout << "\t4. Exit" << endl;
+	cout << "Your Selection: ";
+}
+void factorial() {
+	// I recommend writing your factorial code here
+}
+void arithmetic() {
+	// I recommend writing your arithmetic series code here
+}
+void geometric() {
+	// I recommend writing your geometric series code here
+}
+int main() {
+	int choice;
+	char again;
+	do {
+		printMenu();
+		cin >> choice;
+		// Quit if user chooses to exit (or any invalid choice)
+		if (choice > 3 || choice < 1) {
+			return 0;
+		}
+		else if (choice == 1) {
+			factorial();
+		}
+		else if (choice == 2) {
+			arithmetic();
+		}
+		else if (choice == 3) {
+			geometric();
+		}
+		cout << "Go Again? [Y/N] ";
+		cin >> again;
+	} while (again == 'y' || again == 'Y');
+}
